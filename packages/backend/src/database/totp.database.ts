72d7e9c72b1ba498ffd570f7342561076a409c20
Kevin Hertwig <kevin.hertwig@gmail.com>
2022-11-06 18:46:08 +0100

    refactor: init totp database

--- packages/backend/src/database/totp.database.ts
+++ packages/backend/src/database/totp.database.ts
@@ -3,12 +3,13 @@ import { DB_DIR } from "@config";
 
 export let db;
 
-export const initDB = () =>
-  (db = new JsonDB(
+export const initDB = () => {
+  db = new JsonDB(
     new Config(
       DB_DIR.endsWith("/") ? DB_DIR.concat("totp") : DB_DIR.concat("/totp"),
       true,
       false,
       "/"
     )
-  ));
+  );
+};
