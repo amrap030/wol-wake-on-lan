1e69c395adc1a5515b0a04e40a582fae687a1a0e
Kevin Hertwig <kevin.hertwig@gmail.com>
2022-11-06 13:58:18 +0100

    refactor: default environment variables

--- packages/backend/src/utils/validateEnv.ts
+++ packages/backend/src/utils/validateEnv.ts
@@ -2,10 +2,13 @@ import { cleanEnv, host, port, str } from "envalid";
 
 const validateEnv = () => {
   cleanEnv(process.env, {
-    NODE_ENV: str({ choices: ["development", "test", "production"] }),
-    PORT: port(),
-    LOG_DIR: str(),
-    LOG_FORMAT: str({ choices: ["dev", "combined"] }),
+    NODE_ENV: str({
+      choices: ["development", "test", "production"],
+      default: "development",
+    }),
+    PORT: port({ default: 3000 }),
+    LOG_DIR: str({ default: "/var/log/wol" }),
+    LOG_FORMAT: str({ choices: ["dev", "combined"], default: "dev" }),
     MAC_ADDRESS: str(),
     ORIGIN: process.env.NODE_ENV === "production" ? host() : str(),
     SERVER: str(),
